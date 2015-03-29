05e56b67f9e73864105443ce13ea31727f3407a4
dlewine <dlewine@derryfield.org>
2015-03-29 16:55:23 -0400

    puzzle picture correctly analzsed 3/4 puzzles using 1.5 threshold for stripping excess rows from roi images.

--- src/scrambles/Tile.java
+++ src/scrambles/Tile.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 public class Tile {
     int rotation = 0;
     String[] sides = new String[4];
+    int squareId;
     
     public Tile(ArrayList<String> wordHalves){
         for(int i = 0; i < 4; i++){
             sides[i] = wordHalves.get((int)(8 * Math.random()));
         }
+    }
+    public Tile(String[] sidesArg, int id){
+        //input is an array of the values of the consecutive sides of the tile
+        sides = sidesArg;
+        squareId = id;
     }
     
     public void rotate(int n){
